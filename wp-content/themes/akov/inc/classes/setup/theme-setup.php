ed9dd618347910b774d8f36440bb2bec7bcd2b62
Vadym-Paiuk <vadympaiuk@gmail.com>
2024-05-04 18:00:54 +0300

    Test Deploy

--- wp-content/themes/akov/inc/classes/setup/theme-setup.php
+++ wp-content/themes/akov/inc/classes/setup/theme-setup.php
@@ -108,7 +108,6 @@ class Theme_Setup {
 	public function register_menus() {
 		register_nav_menus( array(
 			'header-menu' => esc_html__( 'Header Menu' ),
-			'footer-menu' => esc_html__( 'Footer Menu' ),
 		) );
 	}
 	
